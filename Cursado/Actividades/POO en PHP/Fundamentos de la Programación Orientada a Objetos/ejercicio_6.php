219fa113ec31d77e9ef8ca013bf03a67df98ccbd
Kevin-Jch <143625990+Kevin-Jch@users.noreply.github.com>
2025-04-25 08:48:55 -0300

    Update ejercicio_6.php

--- Cursado/Actividades/POO en PHP/Fundamentos de la Programación Orientada a Objetos/ejercicio_6.php
+++ Cursado/Actividades/POO en PHP/Fundamentos de la Programación Orientada a Objetos/ejercicio_6.php
@@ -2,16 +2,17 @@
 class Cuenta {
 	public $saldo;
     
-	public function ingresar() {
-    	$this-> saldo += 100;
-        echo "El saldo es de $this->saldo";
+	public function ingresar($ingresar) {
+    	$this-> saldo += $ingresar;
+        echo "Ingreso aceptado, el saldo nuevo es de $this->saldo <br>";
         }
-	public function retirar(){
-    	$this-> saldo -= 100;
-        echo "El saldo es de $this->saldo";
+	public function retirar($retirar){
+    	$this-> saldo -= $retirar;
+        echo "Retiro aceptado, el saldo nuevo es de $this->saldo <br>";
     }
 }
 $cuenta = new Cuenta();
-$cuenta-> saldo = 500;
-$cuenta-> ingresar();
+$cuenta->saldo = 500;
+$cuenta->ingresar(120);
+$cuenta->retirar(120);
 ?>
